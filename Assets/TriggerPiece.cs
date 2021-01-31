bbdec963ee41be2fc29f2af2128f430c0fc2e805
tranhuyssj100 <tranhuyssj100@gmail.com>
2021-01-31 15:06:14 +0700

    fix collider for piece

---
+++ Assets/TriggerPiece.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPiece : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "TriggerPiece")
+            GetComponentInParent<Piece>().canSetPosition = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "TriggerPiece")
+            GetComponentInParent<Piece>().canSetPosition = true;
+    }
+
+
+    public void SetPieceOnGridBoard(bool _value)
+    {
+        GetComponentInParent<Piece>().isOnGridBoard = _value;
+    }
+}
