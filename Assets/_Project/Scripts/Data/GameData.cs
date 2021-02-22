59bba2530fc54378f36733c76a9f264c1bb65e2c
Minh Nhat Hoang <minh.nhat.hoang98@gmail.com>
2021-02-22 11:26:21 +0700

    AdManager

--- Assets/_Project/Scripts/Data/GameData.cs
+++ Assets/_Project/Scripts/Data/GameData.cs
@@ -5,12 +5,16 @@ public static class GameData
     private static int THEME;
     private static int LEVEL;
     private static int IS_SOUND;
+    private static int PREVIEW;
+    private static int AUTO_CORRECT;
 
     static GameData()
     {
         LEVEL= PlayerPrefs.GetInt("level", 0);
         THEME=PlayerPrefs.GetInt("theme", 0);
         IS_SOUND = PlayerPrefs.GetInt("sound", 1);
+        PREVIEW = PlayerPrefs.GetInt("preview", 0);
+        AUTO_CORRECT = PlayerPrefs.GetInt("auto_correct", 0);
     }  
     
     public static int level
@@ -26,7 +30,17 @@ public static class GameData
     public static int isSound
     {
         get { return IS_SOUND; }
-        set { PlayerPrefs.SetInt("theme", (IS_SOUND = value)); }
+        set { PlayerPrefs.SetInt("sound", (IS_SOUND = value)); }
+    }
+    public static int preview
+    {
+        get { return PREVIEW; }
+        set { PlayerPrefs.SetInt("preview", (PREVIEW = value)); }
+    }
+    public static int auto_correct
+    {
+        get { return AUTO_CORRECT; }
+        set { PlayerPrefs.SetInt("auto_correct", (AUTO_CORRECT = value)); }
     }
 
 }
