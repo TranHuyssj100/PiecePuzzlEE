6211c3923bd7c95616b615139d54c681f75f465f
tranhuyssj100 <tranhuyssj100@gmail.com>
2021-02-25 18:22:15 +0700

    test build level

---
+++ Assets/_Project/Scripts/AnswerBuilder/AnswerBuilder.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System.Collections;
+using TMPro.Examples;
+
+public class AnswerBuilder : MonoBehaviour
+{
+    public int size;
+    public int level;
+    public ThemeType theme;
+    [Space()]
+    public int nameAnswer;
+    public List<Object> listTexture;
+    public List<GameObject> listSample;
+    
+
+
+
+    private void Start()
+    {
+        listTexture= LoadTextureFromLevel(0, theme, size);
+        SpawnPiece(listTexture.Count);
+
+
+    }
+
+    public List<Object> LoadTextureFromLevel(int _level, ThemeType _themeType, int _sizeLevel)
+    {
+        string _path = "Themes/" + _themeType.ToString() + "/" + _sizeLevel.ToString() + "x" + _sizeLevel.ToString() + "/" + _level.ToString();
+        Debug.Log(_path);
+        Object[] _textures = Resources.LoadAll(_path, typeof(Texture2D));
+        return _textures.ToList();
+    }
+
+    
+
+    public GameObject CreatePiece(Object _textureObj, GameObject _sampleObj, Vector3 _pointSpawn)
+    {
+        Texture2D _texture = _textureObj as Texture2D;
+        Sprite _sprite = Sprite.Create(_texture, new Rect(0f, 0f, _texture.width, _texture.height), new Vector2(0, 0), 100f);
+        GameObject _spriteObject = new GameObject(_texture.name);
+        GameObject _sampleClone = GameObject.Instantiate(_sampleObj as GameObject, new Vector3(_pointSpawn.x, _pointSpawn.y, 0), Quaternion.identity);
+        _spriteObject.AddComponent<SpriteRenderer>().sprite = _sprite;
+        _spriteObject.GetComponent<SpriteRenderer>().sortingLayerID = SortingLayer.NameToID("Piece");
+        _spriteObject.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        _spriteObject.transform.parent = _sampleClone.transform;
+        Destroy(_sampleClone.GetComponent<Piece>());
+        _sampleClone.AddComponent<PieceBuilder>();
+        //_sampleClone.GetComponent<Piece>().sizeSprite = _spriteObject.Ge
+        _spriteObject.transform.localPosition = Vector3.zero;
+        return _sampleClone;
+    }
+
+    public void SpawnPiece(int _numPiece)
+    {
+        for (int i = 0; i < _numPiece-1; i++)
+        {
+            CreatePiece(listTexture[i], listSample[i], transform.position);
+        }
+    }
+   
+}
