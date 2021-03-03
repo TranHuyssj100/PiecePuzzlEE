cded0eb75c144fcc306cc0484dd589c1cccc9dc4
Minh Nhat Hoang <minh.nhat.hoang98@gmail.com>
2021-03-03 14:03:52 +0700

    Test

---
+++ Assets/_Project/Scripts/Testing/ImageCutterUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ImageCutter)), CanEditMultipleObjects]
+public class ImageCutterUI : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ImageCutter image = (ImageCutter)target;
+
+        if (GUILayout.Button("SpawnGO"))
+        {
+            image.SpawnGObj();
+        }
+        if (GUILayout.Button("Delete"))
+        {
+            image.RemoveAllGObj();
+        }
+        if (GUILayout.Button("Save Prefab"))
+        {
+            image.SaveSelectedAsPrefab();
+        }
+    }
+
+
+}
