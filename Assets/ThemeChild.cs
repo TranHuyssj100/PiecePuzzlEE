5b6243ffeb2f06f815d54c9d17b733ccef3dc93f
tranhuyssj100 <tranhuyssj100@gmail.com>
2021-03-30 17:32:44 +0700

    update ranking level

--- Assets/ThemeChild.cs
+++ Assets/ThemeChild.cs
@@ -10,6 +10,7 @@ public class ThemeChild : MonoBehaviour
     public TextMeshProUGUI titleTxt;
     public TextMeshProUGUI priceTxt;
     public TextMeshProUGUI progressTxt;
+    public Transform rankObj;
     public UnityEngine.UI.Image image;
     public UnityEngine.UI.Button buyBtn, progressBtn;
 
@@ -18,5 +19,21 @@ public class ThemeChild : MonoBehaviour
         GameData.UnlockTheme(index);
         buyBtn.gameObject.SetActive(false);
         progressTxt.text = (GameData.GetCurrentLevelByTheme(index) + 1).ToString() + "/" + DataController.themeData[index].levelCount.ToString();
+    }
+
+    public void SetRankTheme( int sizeLevel)
+    {
+        for (int i = 5; i < rankObj.childCount + 5; i++)
+        {
+            if (i == sizeLevel)
+            {
+                rankObj.GetChild(i - 5).gameObject.SetActive(true);
+            }
+            else
+            {
+                rankObj.GetChild(i - 5).gameObject.SetActive(false);
+
+            }
+        }
     }
 }
